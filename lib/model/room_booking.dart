9108b77a776b7dab62e8f3e32df61efbd94a7553
kmutnb66 <kmutnblubray@gmail.com>
2023-11-06 15:13:59 +0700

    RoomBookingModel add id

--- lib/model/room_booking.dart
+++ lib/model/room_booking.dart
@@ -1,23 +1,26 @@
 import 'dart:convert';
 
-
-
 class RoomBookingModel {
+  String? id;
   String? date;
   String? time;
   String? room_number;
   RoomBookingModel({
+    this.id,
     this.date,
     this.time,
     this.room_number,
   });
+  
 
   RoomBookingModel copyWith({
+    String? id,
     String? date,
     String? time,
     String? room_number,
   }) {
     return RoomBookingModel(
+      id: id ?? this.id,
       date: date ?? this.date,
       time: time ?? this.time,
       room_number: room_number ?? this.room_number,
@@ -26,6 +29,7 @@ class RoomBookingModel {
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'date': date,
       'time': time,
       'room_number': room_number,
@@ -34,6 +38,7 @@ class RoomBookingModel {
 
   factory RoomBookingModel.fromMap(Map<String, dynamic> map) {
     return RoomBookingModel(
+      id: map['id'],
       date: map['date'],
       time: map['time'],
       room_number: map['room_number'],
@@ -45,18 +50,26 @@ class RoomBookingModel {
   factory RoomBookingModel.fromJson(String source) => RoomBookingModel.fromMap(json.decode(source));
 
   @override
-  String toString() => 'RoomBookingModel(date: $date, time: $time, room_number: $room_number)';
+  String toString() {
+    return 'RoomBookingModel(id: $id, date: $date, time: $time, room_number: $room_number)';
+  }
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
   
     return other is RoomBookingModel &&
+      other.id == id &&
       other.date == date &&
       other.time == time &&
       other.room_number == room_number;
   }
 
   @override
-  int get hashCode => date.hashCode ^ time.hashCode ^ room_number.hashCode;
+  int get hashCode {
+    return id.hashCode ^
+      date.hashCode ^
+      time.hashCode ^
+      room_number.hashCode;
+  }
 }
