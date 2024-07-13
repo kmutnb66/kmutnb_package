4186ec000287b4eb9a427383631b0a9630c2900e
kmutnb66 <kmutnblubray@gmail.com>
2024-07-13 11:21:37 +0700

    api notify edit then

--- lib/http/noti.dart
+++ lib/http/noti.dart
@@ -6,13 +6,13 @@ class NotiHttp {
   EnvironmentModel enviroment;
   NotiHttp({required this.enviroment});
   Future<http.Response> list({required String patron_barcode,Map<String, dynamic>? query}) async {
-    var url = Uri.https(enviroment.apiUrls[ApiName.smartapp] ,"/LibMobile/v1/notifies/$patron_barcode " ,query);
+    var url = Uri.https(enviroment.apiUrls[ApiName.smartapp] ,"/LibMobile/v1/notifies/$patron_barcode" ,query);
     var response = await http.get(url, headers: {"authorization": ""});
     return response;
   }
 
    Future<http.Response> read({required String notifyRecId,Map<String, dynamic>? query}) async {
-    var url = Uri.https(enviroment.apiUrls[ApiName.smartapp] ,"/LibMobile/v1/notifies/$notifyRecId " ,query);
+    var url = Uri.https(enviroment.apiUrls[ApiName.smartapp] ,"/LibMobile/v1/notifies/$notifyRecId" ,query);
     var response = await http.put(url, headers: {"authorization": ""});
     return response;
   }
